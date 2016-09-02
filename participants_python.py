2ead6f7e32a13c2b72548ef21393e35a91432897
Kenny Graham <fiikennyg@yahoo.com>
2016-09-02 13:23:18 +1200

    Added some python code

--- participants_python.py
+++ participants_python.py
@@ -1,3 +1,6 @@
 #I would like to write some Python code here that adds two numbers but I don't know how to do that
+def add2(x,y):
+    return x+y
 #I would like to write some code that prints out the name of each participant of this workshop
+print("Kenny Graham")
 #Can you help me using a Pull Requests?
